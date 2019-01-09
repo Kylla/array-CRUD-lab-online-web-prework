40b1f1700dd76abdbb134d33a544bb07adb714d6
Kylla Harris <kylla.harris@gmail.com>
2019-01-09 14:33:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,6 +10,7 @@ def add_element_to_end_of_array(array, element)
  pets = ["cat", "dog", "mouse", "bird"]
   pets.push("snake")
    puts pets.inspect
+   
 end
 
 def add_element_to_start_of_array(array, element)
