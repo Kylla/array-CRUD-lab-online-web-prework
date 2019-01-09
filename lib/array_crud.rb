c3554c68d37c0957401a721246d55bf499d75128
Kylla Harris <kylla.harris@gmail.com>
2019-01-09 14:09:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  pets["cat", "dog", "mouse", "bird"]
+  pets = ["cat", "dog", "mouse", "bird"]
 end
 
 def add_element_to_end_of_array(array, element)
